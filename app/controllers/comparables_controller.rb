1a8b902142db1c4ecde59211bea6aa799b5d265a
Anna Do <annado@gmail.com>
2014-07-06 15:05:30 -0700

    add basic comparables search view

--- app/controllers/comparables_controller.rb
+++ app/controllers/comparables_controller.rb
@@ -1,7 +1,15 @@
 class ComparablesController < ApplicationController
 
   def index
-
   end
 
+  def search
+    @address = search_params[:address]
+    
+  end
+
+  private
+    def search_params
+      params.permit(:address)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'comparables#index'
+
+  get '/comparables/search', to: 'comparables#search'
 end
